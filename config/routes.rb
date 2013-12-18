f50bf0332f9c0981714d174caad5fe954ff77af8
Kristine <kristinka89@gmail.com>
2013-12-18 13:20:30 +0200

    In_Out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
  
+ root :to => 'static_pages#home'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
 
   #get "static_pages/home"
   #get "static_pages/help"
@@ -8,11 +11,13 @@ SampleApp::Application.routes.draw do
   #get "static_pages/contact"
 
   #root to: 'static_pages#home'
-  root :to => 'static_pages#home'
+  
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -59,7 +59,9 @@ describe "User pages" do
 
         it {should have_selector('title', text: user_name)}
         it {should have_selector('div.alert.alert-success', text: 'Welcome' )}
+        it {should have_link('Sign out')}
       end
+
     end
   end
 end
