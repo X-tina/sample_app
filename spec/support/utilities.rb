59aeaa4f899ac36bfa1c9d818cfdc980160383a4
Kristine <kristinka89@gmail.com>
2013-12-19 13:43:43 +0200

    Edit

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -10,9 +10,18 @@ def full_title(page_title)
 end
 
 def valid_signin(user)
-	fill_in "Email",    with: user.email
-  	fill_in "Password", with: user.password
-  	click_button "Sign in"
+	 fill_in "Email",    with: user.email
+   fill_in "Password", with: user.password
+   click_button "Sign in"
+end
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Вход без Capybara.
+  cookies[:remember_token] = user.remember_token
 end
 
 RSpec::Matchers.define :have_error_message do |message|
@@ -20,3 +29,4 @@ RSpec::Matchers.define :have_error_message do |message|
     page.should have_selector('div.alert.alert-error', text: message)
   end
 end
+
